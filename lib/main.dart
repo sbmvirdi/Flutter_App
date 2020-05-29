02d5ecb254db95950c7336fd6a5e4b9ba5e10a69
sbmvirdi <shubhamvirdic3@gmail.com>
2020-05-30 01:06:26 +0530

    Added: Image Asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
-
 // The main function is the starting point to the flutter App
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
-      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey[600],
+          ),
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          )),
     ),
   );
 }
